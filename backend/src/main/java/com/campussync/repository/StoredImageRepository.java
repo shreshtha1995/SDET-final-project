082f74e0a5cc7a766906e369041d46eb1ba70bcb
Srivastava <2492279@cognizant.com>
2026-06-21 13:20:21 +0530

    final changes

--- backend/src/main/java/com/campussync/repository/StoredImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.campussync.repository;
-
-import com.campussync.model.StoredImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoredImageRepository extends JpaRepository<StoredImage, Long> {
-}
